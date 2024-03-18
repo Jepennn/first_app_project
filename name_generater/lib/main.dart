2f44b3c7fd77d6f8d1634061337160230e040469
Jepennn <jeppehesselgren@gmail.com>
2024-03-18 09:24:12 +0100

    added some functionality

--- name_generater/lib/main.dart
+++ name_generater/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:faker/faker.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         title: 'Namer App',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Color.fromRGBO(20, 250, 93, 1)),
         ),
         home: MyHomePage(),
       ),
@@ -26,27 +27,77 @@ class MyApp extends StatelessWidget {
 }
 
 class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
+  //var current = WordPair.random();
+  var current = faker.person.firstName();
+
+  void getNext() {
+    //current = WordPair.random();
+    current = faker.person.firstName();
+    notifyListeners();
+
+    //Like button logic starts here
+    var favorites = <String>[];
+
+    void toogleFavorite(){
+      if (favorites.contains(current)){
+        favorites.remove(current);
+      } else {
+        favorites.add(current);
+    }
+    notifyListeners();
+    }
+  }
 }
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var name = appState.current;
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('A cool idea:'),
-          Text(appState.current.asLowerCase),
-          // ↓ Add this.
-          ElevatedButton(
-            onPressed: () {
-              print('button pressed!');
-            },
-            child: Text('Next'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          
+          children: [
+            BigCard(name: name),
+            SizedBox(height: 30.0),
+            // ↓ Next Bottom.
+            ElevatedButton(
+              onPressed: () {
+                appState.getNext();
+              },
+              child: Text('Next'),
             ),
-        ],
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.name,
+  });
+
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(name, style: style),
       ),
     );
   }
